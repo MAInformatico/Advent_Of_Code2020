52a14f401259efd954b03579d17c0b9a7c9d53a0
Mike <magutierrezsistemas@gmail.com>
2020-12-20 09:03:13 +0100

    Day19

---
+++ day19.py
@@ -0,0 +1,44 @@
+import re
+
+def generateRegex(rule, rules, depth, maxdepth):
+    if maxdepth and depth > maxdepth:
+        return ""
+    if "a" in rule:
+        return "a"
+    elif "b" in rule:
+        return "b"
+    elif "|" in rule:
+        left, right = re.findall(r'(.+) \| (.+)', rule)[0]
+        return "(?:" + generateRegex(left, rules,  depth + 1, maxdepth) + \
+            "|" + generateRegex(right, rules,  depth + 1, maxdepth) + ")"
+    else:
+        regex = ""
+        index = re.findall(r'(\d+)', rule)
+        for i in index:
+            regex += generateRegex(rules[int(i)],
+                                    rules,  depth + 1, maxdepth)
+        return regex
+
+
+def Matches(string, regexpression):
+    count = 0
+    for iterator in string:
+        if regexpression.match(iterator):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':    
+
+    with open("inputDay19.txt") as f:
+        data = f.read()
+
+    rulestext = re.findall(r'(\d+)\: (.+)', data)
+    rules = [[]] * (len(rulestext))
+    for rule in rulestext:
+        rules[int(rule[0])] = rule[1]
+    string = re.findall(r'([a|b]\w+)', data)
+    
+    regexpression = re.compile("^" + generateRegex(rules[0], rules, 0, 0) + "$")
+    print(Matches(string, regexpression))
+    
